a1456e56af3cd231ca9ab62b245ddb4ea4d974d5
Javier Naranjo <javnarsan@gmail.com>
2022-11-03 12:22:00 +0100

    Dependencias para poder usar font awesome y derivados

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -12,10 +12,13 @@
                         @csrf
 
                         <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+                            <label for="name" class="col-md-4 col-form-label text-md-right">
+                                <i class="fa fa-sharp fa-solid fa-user-bounty-hunter"></i>
+                            </label>
+                            
 
                             <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                                <input id="name" placeholder="Name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
 
                                 @error('name')
                                     <span class="invalid-feedback" role="alert">
@@ -26,10 +29,10 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="surname" class="col-md-4 col-form-label text-md-right">{{ __('Surname') }}</label>
+                            <label for="surname" class="col-md-4 col-form-label text-md-right"><i class="fa fa-sharp fa-solid fa-user-bounty-hunter"></i></label>
 
                             <div class="col-md-6">
-                                <input id="surname" type="text" class="form-control @error('surname') is-invalid @enderror" name="surname" value="{{ old('surname') }}" required autocomplete="surname" autofocus>
+                                <input id="surname" placeholder="Surname" type="text" class="form-control @error('surname') is-invalid @enderror" name="surname" value="{{ old('surname') }}" required autocomplete="surname" autofocus>
 
                                 @error('name')
                                     <span class="invalid-feedback" role="alert">
@@ -40,10 +43,12 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-right">
+                                {{ __('E-Mail Address') }}
+                            </label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                                <input id="email" placeholder="Email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
@@ -52,21 +57,29 @@
                                 @enderror
                             </div>
                         </div>
-                        <div class="form-group">
-                            <select name="cicle_id" class="form-control{{ $errors->has('cicle_id') ? ' is-invalid':''}}">
-                                <option value="" selected disabled hidden>Choose a cicle</option>
-                                @foreach($cicles as $cicle)
-                                    <option value="{{$cicle->id}}">{{$cicle->name}}</option>
-                                @endforeach
-                                <option value="" selected disabled hidden>Choose a cicle</option>
-                            </select>
+                        <div class="form-group row">
+                            <label for="form" class="col-md-4 col-form-label text-md-right">
+                                {{ __('Cicle') }}
+                            </label>
+
+                            <div class="col-md-6">    
+                                <select name="cicle_id" class="form-control{{ $errors->has('cicle_id') ? ' is-invalid':''}}">
+                                    <option value="" selected disabled hidden>Choose a cicle</option>
+                                    @foreach($cicles as $cicle)
+                                        <option value="{{$cicle->id}}">{{$cicle->name}}</option>
+                                    @endforeach
+                                    <option value="" selected disabled hidden>Choose a cicle</option>
+                                </select>
+                            </div>
                         </div>
 
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                            <label for="password" class="col-md-4 col-form-label text-md-right">
+                                <i class="fa fa-solid fa-user-secret"></i>
+                            </label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                                <input id="password" placeholder="Password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
@@ -77,10 +90,12 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">
+                                {{ __('Confirm Password') }}
+                            </label>
 
                             <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                                <input id="password-confirm" placeholder="Confirm password" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
                             </div>
                         </div>
 
